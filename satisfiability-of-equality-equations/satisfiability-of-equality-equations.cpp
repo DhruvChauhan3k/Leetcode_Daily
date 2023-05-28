a227223ce057fc90aa0b15aee2fa1e6631036dd0
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2023-05-29 01:19:28 +0530

    Time: 10 ms (24.36%) | Memory: 12.8 MB (5.11%) - LeetSync

---
+++ satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -0,0 +1,78 @@
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+class Solution {
+public:
+    bool equationsPossible(vector<string>& eq) {
+        vector<string>e;
+        for(auto it:eq)
+        {
+            if(it[1]=='=')e.push_back(it);
+        }
+        for(auto it:eq)
+        {
+            if(it[1]=='!')e.push_back(it);
+        }
+        // return false;
+        DisjointSet ds(200);
+        for(auto it:e)
+        {
+            char a=it[0],b=it[3],c=it[1];
+            if(c=='!')
+            {
+                if(ds.findUPar(a)==ds.findUPar(b))return false;
+            }
+            else
+            {
+                ds.unionBySize(a,b);
+            }
+        }
+        return true;
+    }
+};
