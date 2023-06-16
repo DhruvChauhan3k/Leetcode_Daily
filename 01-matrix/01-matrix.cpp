927ef0cc2dc9b2b433b4c15f0829e482f6d1f0ff
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2023-06-16 19:33:00 +0530

    Time: 164 ms (11.42%) | Memory: 31.7 MB (62.13%) - LeetSync

--- 01-matrix/01-matrix.cpp
+++ 01-matrix/01-matrix.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({0, {i, j}});
+        int n=mat.size(),m=mat[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+          priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    dist[i][j]=0;
+                    q.push({0,{i,j}});
                 }
             }
         }
-        
-        while (!q.empty()) {
-            int dis = q.top().first;
-            int x = q.top().second.first;
-            int y = q.top().second.second;
+        while(!q.empty())
+        {
+            int dis=q.top().first;
+            int x=q.top().second.first;
+            int y=q.top().second.second;
             q.pop();
-            int dx[] = {1, -1, 0, 0};
-            int dy[] = {0, 0, 1, -1};
-            
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-                
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (dis + 1 < dist[nx][ny]) {
-                        dist[nx][ny] = dis + 1;
-                        q.push({dis + 1, {nx, ny}});
+            int i[]={1,-1,0,0};
+            int j[]={0,0,1,-1};
+            for(int k=0;k<4;k++)
+            {
+                if(x+i[k]>=0 and x+i[k]<n and y+j[k]>=0 and y+j[k]<m)
+                {
+                    if(dis+1<dist[x+i[k]][y+j[k]])
+                    {
+                        dist[x+i[k]][y+j[k]]=dis+1;
+                        q.push({dis+1,{x+i[k],y+j[k]}});
                     }
                 }
             }
+
         }
-        
         return dist;
     }
 };
