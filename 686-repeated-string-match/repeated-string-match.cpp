0ae605bb93db1f9b5e6579eb1ae63193eee3710e
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2023-08-02 02:07:05 +0530

    Time: 4 ms (90.69%) | Memory: 7.4 MB (40.69%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,42 +1,44 @@
 class Solution {
+private:
+    int BASE = 1000000;
 public:
-    
-    bool check(string &a, string &b)
-    {
-        int m=a.size();
-        int n=b.size();
-        
-        for(int i=0;i<=m-n;i++)
-        {
-            int j;
-            for(j=0;j<n;j++){
-                if(a[i+j]!=b[j])
-                    break;
-            }
-            if(j==n)
-                return true;
+    int repeatedStringMatch(string A, string B) {
+        if(A == B) return 1;
+        int count = 1;
+        string source = A;
+        while(source.size() < B.size()){
+            count++;
+            source+=A;
         }
-        return false;
+        if(source == B) return count;
+        if(Rabin_Karp(source,B) != -1) return count;
+        if(Rabin_Karp(source+A,B) != -1) return count+1;
+        return -1;
     }
-    
-    int repeatedStringMatch(string a, string b) 
-    {
-        int m=a.size();
-        int n=b.size();
-        
-        int ans=1;
-        string tmp=a;
-        while(a.size() < b.size()){
-            a += tmp;
-            ans++;
+    int Rabin_Karp(string source, string target){
+        if(source == "" or target == "") return -1;
+        int m = target.size();
+        int power = 1;
+        for(int i = 0;i<m;i++){
+            power = (power*31)%BASE;
         }
-        
-        if(check(a,b)){
-            return ans;
+        int targetCode = 0;
+        for(int i = 0;i<m;i++){
+            targetCode = (targetCode*31+target[i])%BASE;
         }
-        a += tmp;
-        if(check(a,b)){
-            return ans+1;
+        int hashCode = 0;
+        for(int i = 0;i<source.size();i++){
+            hashCode = (hashCode*31 + source[i])%BASE;
+            if(i<m-1) continue;
+            if(i>=m){
+                hashCode = (hashCode-source[i-m]*power)%BASE;
+            }
+            if(hashCode<0)
+                hashCode+=BASE;
+            if(hashCode == targetCode){
+                if(source.substr(i-m+1,m) == target)
+                    return i-m+1;
+            }
         }
         return -1;
     }
