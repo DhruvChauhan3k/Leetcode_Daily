a16da026ebd53bc87a2e3b678a0503534f162f68
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2023-08-07 11:57:14 +0530

    Time: 2 ms (50.77%) | Memory: 6 MB (18.33%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n==0)return 1;
+        if(n==1)return 10;
+        vector<int>dp;
+        int final=10;
+        for(int k=2;k<=n;k++)
+        {
+            int ans=9;
+            int prev=9;
+            for(int o=1;o<k;o++)
+            {
+                ans=ans*prev;
+                prev--;
+            }
+            final+=ans;
+        }
+        return final;
+    }
+};
