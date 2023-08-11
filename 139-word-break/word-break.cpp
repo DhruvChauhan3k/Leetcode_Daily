e349fae9fea299af79864df24122034b1036b97e
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2023-08-11 16:33:29 +0530

    Time: 86 ms (5.04%) | Memory: 43.6 MB (5.01%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-bool check(int i,string &s, unordered_map<string,int>&m,string curr, unordered_map<int,unordered_map<string,int>>&dp)
+bool check(int i,string &s, unordered_map<string,int>&m,string curr, vector<unordered_map<string,bool>>&dp)
 {
     curr+=s[i];
     if(dp[i].find(curr)!=dp[i].end())return dp[i][curr];
@@ -19,8 +19,7 @@ bool check(int i,string &s, unordered_map<string,int>&m,string curr, unordered_m
 }
     bool wordBreak(string s, vector<string>& w) {
         unordered_map<string,int>m;
-        // vector<unordered_map<string,bool>>dp(s.size());
-        unordered_map<int,unordered_map<string,int>>dp;
+        vector<unordered_map<string,bool>>dp(s.size());
         for(auto it:w)m[it]++;
         string curr;
         return check(0,s,m,curr,dp);
