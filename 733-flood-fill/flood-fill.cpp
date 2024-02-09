48f8817dd68f70d0c6f29c5f8a38af6f490b12bc
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2024-02-09 23:16:42 +0530

    Time: 8 ms (53.46%) | Memory: 16.6 MB (22.40%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,25 @@
+int n,m;
+class Solution {
+public:
+       void dfs(vector<vector<int>>&image,int i,int j,int oc,int &change)
+       {
+           image[i][j]=change;
+            int dx[]={1,-1,0,0};
+            int dy[]={0,0,1,-1};
+            for(int k=0;k<4;k++)
+            {
+                int x=i+dx[k],y=j+dy[k];
+                if(x>=0 and x<n and y>=0 and y<m and image[x][y]==oc)
+                {
+                     dfs(image,x,y,oc,change);
+                }
+            }
+       }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        n=image.size(),m=image[0].size();
+        // vector<vector<int>>visited(n,vector<int>(m,0));
+        if(image[sr][sc]==color)return image;
+        dfs(image,sr,sc,image[sr][sc],color);
+        return image;
+    }
+};
