566eaf00648d812be97d7695f5bda8d0262769cd
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2024-03-09 16:59:02 +0530

    Time: 24 ms (95.81%) | Memory: 40.3 MB (32.24%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    bool check(vector<int>&arr,int start,vector<int>&visit)
+    bool check(vector<int>&arr,int start,vector<int>&visit,vector<int>&dp)
     {
+        if(dp[start]!=-1)return dp[start];
         visit[start]=1;
         if(arr[start]==0)return true;
         int a=start+arr[start],b=start-arr[start];
+        bool ak=false;
         if(a<arr.size() and visit[a]==0)
         {
-            if(check(arr,a,visit))return true;
+            if(check(arr,a,visit,dp))ak=true;
         }
         if(b>=0 and visit[b]==0)
         {
-            if(check(arr,b,visit))return true;
+            if(check(arr,b,visit,dp))ak= true;
         }
         visit[start]=0;
-        return false;
+        return dp[start]=ak;
     }
     bool canReach(vector<int>& arr, int start) {
         vector<int>visit(arr.size(),0);
-        return check(arr,start,visit);
+        vector<int>dp(arr.size(),-1);
+        return check(arr,start,visit,dp);
     }
 };
