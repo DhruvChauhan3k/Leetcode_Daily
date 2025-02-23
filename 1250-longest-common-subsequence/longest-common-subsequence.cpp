c983efd7c3df45d19eb2e66f5ee971767639f7d4
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-02-23 20:27:10 +0530

    Time: 27 ms (56.38%) | Memory: 27.5 MB (28.54%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int check(int i,string &text1,int j,string&text2,vector<vector<int>>&dp){
-        if(i==text1.size() or j==text2.size())return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(text1[i]==text2[j]){
-            return dp[i][j]= 1+check(i+1,text1,j+1,text2,dp);
-        }
-        return dp[i][j]= max(check(i+1,text1,j,text2,dp),check(i,text1,j+1,text2,dp));
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return check(0,text1,0,text2,dp);
+        for(int i=0;i<=text2.size();i++){
+          dp[n][i]=0;
+        }
+        for(int j=0;j<=text1.size();j++){
+          dp[j][m]=0;
+        }
+
+        int ans=0;
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--)
+            {
+                if(dp[i][j]!=-1)continue;
+                if(text1[i]==text2[j]){
+                    dp[i][j]= 1+dp[i+1][j+1];
+                    ans=max(ans,dp[i][j]);
+                    continue;
+                }
+                dp[i][j]= max(dp[i+1][j],dp[i][j+1]);
+                ans=max(dp[i][j],ans);
+            }
+        }
+
+        return dp[0][0];
     }
 };
