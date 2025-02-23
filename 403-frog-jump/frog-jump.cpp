19d13d101b49af8e52171b341a563486123357fd
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-02-23 10:59:05 +0530

    Time: 1621 ms (5.00%) | Memory: 216.9 MB (21.12%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -31,7 +31,31 @@ public:
         if (stones[1] != 1) return false;  // The first jump must be 1
         
         vector<vector<int>> dp(n, vector<int>(n + 1, -1));  // DP table
-        
-        return check(1, stones, dp, 1, m);  // Start from the second stone with a jump of 1
+
+
+
+        for(int i=0;i<=n;i++){
+            dp[n-1][i]=1;
+        }
+
+        for(int i=n-1;i>=1;i--){
+            for(int k=n-1;k>=1;k--){
+
+            int ans=0;
+            if (dp[i][k] != -1) continue; 
+
+            for (int step : {k - 1, k, k + 1}) {
+            if (step > 0 && m.find(stones[i] + step) != m.end()) {
+                int next_index = m[stones[i] + step];
+                if (next_index > i) {  // Ensure we are moving forward
+                    ans |= dp[next_index][step];
+                }
+            }
+        }
+        dp[i][k] = ans;
+            }
+        }
+  
+        return dp[1][1];  // Start from the second stone with a jump of 1
     }
 };
