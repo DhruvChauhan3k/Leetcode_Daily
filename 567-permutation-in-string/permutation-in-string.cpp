4711dd255dcbebcf82c9c534a42616538a03b6ed
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-06-05 03:26:58 +0530

    Time: 31 ms (31.10%) | Memory: 32.4 MB (12.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
+    bool areVectorsEqual(vector<int> a, vector<int> b){
+        for(int i=0; i<26; i++){
+            if(a[i]!=b[i]) return false;
+        }
+        return true;
+    }
 public:
     bool checkInclusion(string s1, string s2) {
-        int s=0;
-        unordered_map<char,int>m,v;
-        for(auto it:s1)m[it]++;
-        int n=s2.size(),m1=s1.size();
-        int e=0;
-        while(e<n)
-        {
-            v[s2[e]]++;
-            while(m.find(s2[e])!=m.end() and v[s2[e]]>m[s2[e]])
-            {
-                v[s2[s]]--;
-                s++;
+        if(s2.size()<s1.size()) return false;
+        vector<int> freqS1(26, 0);
+        for(char c: s1) freqS1[c-'a']++;
+        
+        vector<int> freqS2(26, 0);
+        int i=0, j=0;
+        
+        while(j<s2.size()){
+            freqS2[s2[j]-'a']++;
+            
+            if(j-i+1==s1.size()){
+                if(areVectorsEqual(freqS1, freqS2)) return true;
             }
-            if(m.find(s2[e])==m.end())
-            {
-                while(s<=e)
-                {
-                    v[s2[s]]--;
-                    s++;
-                }
+            
+            if(j-i+1<s1.size()) j++;
+            else{
+                freqS2[s2[i]-'a']--;
+                i++;
+                j++;
             }
-            if((e-s)+1==m1)return true;
-            e++;
         }
         return false;
     }
