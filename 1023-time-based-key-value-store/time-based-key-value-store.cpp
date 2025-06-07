8cc1f0ace6ad7618835f161ea2870f8f1f72edb3
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-06-08 01:13:35 +0530

    Time: 31 ms (95.28%) | Memory: 136.6 MB (79.91%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,22 +1,38 @@
 class TimeMap {
-public:
-    unordered_map<string,map<int,string>>m;
-    TimeMap() {
-        return;
+private:
+    unordered_map<string, vector<pair<string, int>>> structure; 
+    
+    string search(vector<pair<string, int>> &temp, const int &timestamp) {
+        int low = 0;
+        int high = temp.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (temp[mid].second > timestamp) {
+                high = mid - 1;
+            } else if (temp[mid].second < timestamp) {
+                low = mid + 1;
+            } else {
+                return temp[mid].first;
+            }
+        }
+        
+        return high >= 0 ? temp[high].first : "";
     }
-    void set(string key, string value, int time) {
-     m[key][time]=value;   
+public:
+    TimeMap() {
+        
     }
     
-    string get(string key, int time) {
-        auto it = m[key].upper_bound(time);
-      return it == m[key].begin() ? "" : prev(it)->second;
+    void set(string key, string value, int timestamp) {
+        structure[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        if (structure.find(key) == structure.end()) {
+            return "";
+        }
+        return search(structure[key], timestamp);
     }
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
