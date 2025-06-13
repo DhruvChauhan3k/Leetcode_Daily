e37afc000d75637fe16b91ff36da6c39827145db
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-06-14 03:37:09 +0530

    Time: 47 ms (60.60%) | Memory: 71.6 MB (32.69%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool pal(string&s)
+    {
+        int st=0,e=s.size()-1;
+        while(st<e)
+        { 
+            if(s[st]!=s[e])
+            {
+                return false;
+            }
+            st++;
+            e--;
+        }
+        return true;
+    }
+    void check(int i,string &s, vector<vector<string>>&ans,vector<string>&temp,string st)
+    {
+        if(i==s.size())
+        {
+            if(st.size()>0 and pal(st))
+            {
+                temp.push_back(st);
+                ans.push_back(temp);
+                temp.pop_back();
+            }
+            return;
+        }
+        st+=s[i];
+        check(i+1,s,ans,temp,st);
+        if(pal(st))
+        {
+            temp.push_back(st);
+            check(i+1,s,ans,temp,"");
+            temp.pop_back();
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>temp;
+        string st;
+        check(0,s,ans,temp,st);
+        return ans;
+    }
+};
