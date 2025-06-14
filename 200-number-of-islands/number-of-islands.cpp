859202645c551c39a72f238356b7a9ddf0a15394
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-06-14 18:39:54 +0530

    Time: 20 ms (94.42%) | Memory: 16.3 MB (77.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void check(int i,int j,vector<vector<int>>&visited,vector<vector<char>>&grid)
+    void check(int i,int j,vector<vector<char>>&grid)
     {
         int dx[]={1,-1,0,0};
         int dy[]={0,0,1,-1};
         for(int k=0;k<4;k++)
         {
             int x=i+dx[k],y=j+dy[k];
-            if(x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and visited[x][y]==0 and grid[x][y]=='1')
+            if(x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y]=='1')
             {
-                visited[x][y]=1;
-                check(x,y,visited,grid);
+                grid[x][y]=1;
+                check(x,y,grid);
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
      int n=grid.size(),m=grid[0].size();
-     vector<vector<int>>visited(n,vector<int>(m,0));
      int ans=0;
      for(int i=0;i<n;i++)
      {
          for(int j=0;j<m;j++)
          {
-             if(visited[i][j]==0 and grid[i][j]=='1')
+             if(grid[i][j]=='1')
              {
-               visited[i][j]=1;
-               check(i,j,visited,grid);  
+               grid[i][j]=1;
+               check(i,j,grid);  
                ans++;
              }
          }
