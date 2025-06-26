b3ca41e06bb770a1b03128fd0777b70fa6792cba
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-06-26 21:35:52 +0530

    Time: 11 ms (32.56%) | Memory: 22 MB (22.71%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(vector<string>&ans,stack<string>&st,unordered_map<string,multiset<string>>&adj,string begin)
+    {
+        if(adj[begin].size()==0)
+        {
+            ans.push_back(begin);
+            return;
+            // st.pop();
+        }
+        else
+        {
+            while(adj[begin].size())
+            {
+                auto it=adj[begin].begin();
+                string n=*it;
+                adj[begin].erase(it);
+                dfs(ans,st,adj,n);
+            }
+        }
+        ans.push_back(begin);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+       unordered_map<string,multiset<string>>adj;
+       for(auto it:tickets)
+       {
+           adj[it[0]].insert(it[1]);
+       }
+       stack<string>st;
+       vector<string>ans;
+       dfs(ans,st,adj,"JFK");
+       reverse(ans.begin(),ans.end());
+       return ans;
+    }
+};
