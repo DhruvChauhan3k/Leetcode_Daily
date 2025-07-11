c27f2084ec5fb52b3777b937fa74a7c521b3652f
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-07-11 10:42:31 +0530

    Time: 79 ms (63.21%) | Memory: 172.8 MB (94.30%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,16 +1,16 @@
 class LRUCache {
 public:
-    class Node{
-        public: 
-            int key;
-            int val;
-            Node* prev;
-            Node* next;
+    class Node {
+    public: 
+        int key;
+        int val;
+        Node* prev;
+        Node* next;
 
-            Node(int key, int val){
-                this->key = key;
-                this->val = val;
-            }
+        Node(int key, int val) {
+            this->key = key;
+            this->val = val;
+        }
     };
 
     Node* head = new Node(-1, -1);
@@ -21,56 +21,55 @@ public:
 
     LRUCache(int capacity) {
         cap = capacity;
-        head -> next = tail;
-        tail -> prev = head;
+        head->next = tail;
+        tail->prev = head;
     }
 
-    void addNode(Node* newnode){
-        Node* temp = head -> next;
-
-        newnode -> next = temp;
-        newnode -> prev = head;
-
-        head -> next = newnode;
-        temp -> prev = newnode;
+    void addNode(Node* newnode) {
+        Node* temp = tail->prev;
+        temp->next = newnode;
+        newnode->prev = temp;
+        newnode->next = tail;
+        tail->prev = newnode;
     }
 
-    void deleteNode(Node* delnode){
-        Node* prevv = delnode -> prev;
-        Node* nextt = delnode -> next;
+    void deleteNode(Node* delnode) {
+        Node* prevv = delnode->prev;
+        Node* nextt = delnode->next;
 
-        prevv -> next = nextt;
-        nextt -> prev = prevv;
+        prevv->next = nextt;
+        nextt->prev = prevv;
     }
-    
+
     int get(int key) {
-        if(m.find(key) != m.end()){
+        if (m.find(key) != m.end()) {
             Node* resNode = m[key];
-            int ans = resNode -> val;
+            int ans = resNode->val;
 
-            m.erase(key);
             deleteNode(resNode);
             addNode(resNode);
 
-            m[key] = head -> next;
             return ans;
         }
         return -1;
     }
-    
+
     void put(int key, int value) {
-        if(m.find(key) != m.end()){
+        if (m.find(key) != m.end()) {
             Node* curr = m[key];
-            m.erase(key);
             deleteNode(curr);
+            addNode(curr);
+            curr->val = value;
+            return;
         }
 
-        if(m.size() == cap){
-            m.erase(tail -> prev -> key);
-            deleteNode(tail -> prev);
+        if (m.size() == cap) {
+            Node* lru = head->next;     // ✅ LRU is right after head
+            m.erase(lru->key);
+            deleteNode(lru);
         }
-
-        addNode(new Node(key, value));
-        m[key] = head -> next;
+            Node *tmp=new Node(key,value);
+            addNode(tmp);
+            m[key]=tmp;
     }
 };
