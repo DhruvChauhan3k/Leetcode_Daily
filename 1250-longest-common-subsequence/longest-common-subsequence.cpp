0bcf6cbe48f8490a980ca5bab32ba2e747e3c59f
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-08-15 21:03:55 +0530

    Time: 27 ms (55.84%) | Memory: 27.5 MB (51.75%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -11,8 +11,8 @@ public:
           dp[j][m]=0;
         }
 
-        for(int i=n;i>=0;i--){
-            for(int j=m;j>=0;j--)
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--)
             {
                 if(dp[i][j]!=-1)continue;
                 if(text1[i]==text2[j]){
