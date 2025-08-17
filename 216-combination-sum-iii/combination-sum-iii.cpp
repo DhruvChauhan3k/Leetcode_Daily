c729a090940eb834e7c65fc9fea5faad35019287
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-08-17 11:19:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (59.77%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void check(vector<vector<int>>&ans,vector<int>&temp,int k,int n,int i)
+{
+    if(n==0 and k==0)
+    {
+        ans.push_back(temp);
+        return;
+    }
+    if(n<=0 || k<=0)return;
+    if(i>9 or n<=0 or k<=0)return;
+    check(ans,temp,k,n,i+1);
+    temp.push_back(i);
+    check(ans,temp,k-1,n-i,i+1);
+    temp.pop_back();
+}
+    vector<vector<int>> combinationSum3(int k, int n) {
+      vector<vector<int>>ans;
+      vector<int>temp;
+      check(ans,temp,k,n,1);  
+      return ans;
+    }
+};
