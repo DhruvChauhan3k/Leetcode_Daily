489b17589b5bff20e3b650be7f2792ef680f6308
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-10-01 19:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (50.52%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<int>> dp;
-
-    int dfs(int i, int j, vector<vector<int>>& dungeon) {
-        // Out of bounds → impossible, return large value
-        if (i >= m || j >= n) return INT_MAX;
-
-        // If already computed
-        if (dp[i][j] != -1) return dp[i][j];
-
-        // Base case: princess cell (bottom-right)
-        if (i == m - 1 && j == n - 1) {
-            return dp[i][j] = max(1, 1 - dungeon[i][j]);
-        }
-
-        // Recursive calls → go right or down
-        int right = dfs(i, j + 1, dungeon);
-        int down  = dfs(i + 1, j, dungeon);
-
-        // Choose min health needed from right or down
-        int need = min(right, down) - dungeon[i][j];
-
-        // Store and return
-        return dp[i][j] = max(1, need);
+    int getVal(vector<vector<int>> &mat, vector<vector<int>> &dp , int i=0, int j=0)
+    {
+        int n = mat.size();
+        int m = mat[0].size();    
+        
+        if(i == n || j == m)    return 1e9; 
+        
+        if(i == n-1 and j == m-1)
+            return (mat[i][j] <= 0) ? -mat[i][j] + 1 : 1;
+        
+        /// if we know the answer for this cell then no need to recalculate those, simply return those values 
+        if( dp[i][j] != 1e9)
+            return dp[i][j];
+        
+        int IfWeGoRight = getVal(mat , dp , i , j+1);
+        int IfWeGoDown = getVal(mat , dp , i+1 , j);
+        
+        int minHealthRequired =  min(IfWeGoRight , IfWeGoDown) -mat[i][j];
+        
+        /// before returning the values, we must store the answers for this cell which we hacve calculated
+        /// in next recurssive call this value will be used to save some computation, aka repetative work which we are doing.
+        dp[i][j] = ( minHealthRequired <= 0 ) ? 1 : minHealthRequired;      
+        return dp[i][j];
     }
-
+    
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        m = dungeon.size();
-        n = dungeon[0].size();
-        dp.assign(m, vector<int>(n, -1));
-
-        return dfs(0, 0, dungeon);
+        
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+        
+        vector<vector<int>> dp(n , vector<int>(m , 1e9));
+        
+        return getVal(dungeon, dp);     
     }
 };
