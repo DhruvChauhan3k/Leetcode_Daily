e14d15c6f507d6a3730e9fd148fdad3f085d55e3
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2025-10-22 20:21:59 +0530

    Time: 2219 ms (11.85%) | Memory: 388 MB (24.65%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        unordered_map<int,int>hash,mp;
+        set<int>points;
+        for(auto &it:nums){
+            hash[it]++;
+            mp[it-k]++;
+            mp[it+k+1]--;
+            points.insert(it);
+            points.insert(it-k);
+            points.insert(it+k+1);
+        }
+        int ans=1;
+        int sum=0;
+        for(auto &it:points){
+            sum+=mp[it];
+            ans=max(ans,hash[it]+min(sum-hash[it],numOperations));
+        }
+        return ans;
+    }
+};
