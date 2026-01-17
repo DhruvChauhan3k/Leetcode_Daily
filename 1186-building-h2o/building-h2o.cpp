a76f3f91365872ea4e53aa60e186f343b83dbcd5
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2026-01-17 11:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (19.28%) - LeetSync

---
+++ 1186-building-h2o/building-h2o.cpp
@@ -0,0 +1,35 @@
+class H2O {
+private:
+    mutex mtx;
+    condition_variable cv;
+    int hydrogen_count = 0;
+
+public:
+    H2O() {}
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        unique_lock<mutex> lck(mtx);
+
+        cv.wait(lck,[this](){return hydrogen_count<2;});
+
+        hydrogen_count++;
+        releaseHydrogen();
+        
+        // If two hydrogen atoms are ready, wake up oxygen
+        cv.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        unique_lock<mutex> lck(mtx);
+
+        // Wait until exactly 2 hydrogens are available
+        cv.wait(lck,[this](){return hydrogen_count>=2;});
+
+        releaseOxygen();
+
+        // Reset for the next water molecule
+        hydrogen_count = 0;
+        // Notify waiting hydrogen threads
+        cv.notify_all(); 
+    }
+};
