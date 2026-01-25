298d2ac3f46da7ef11b1a1b0ad276fdf0e957443
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2026-01-25 12:19:20 +0530

    Time: 22 ms (99.04%) | Memory: 101.1 MB (90.07%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string,vector<string>>m;
-        unordered_map<string,int>n;
-        for(int i=0;i<recipes.size();i++)
-        {
-            for(int j=0;j<ingredients[i].size();j++)
-            {
-                m[ingredients[i][j]].push_back(recipes[i]);
-                n[recipes[i]]++;
-            }
-        }
-        queue<string>q;
-        for(auto it:supplies)
-        {
-            q.push(it);
-            n[it]=0;
-        }
-        vector<string>ans;
-        while(!q.empty())
-        {
-            string temp=q.front();
-            q.pop();
-            for(auto it:m[temp])
-            {
-                n[it]--;
-                if(n[it]==0)
-                {
-                    ans.push_back(it);
-                    q.push(it);
+
+     void dfs(int i,unordered_map<string,int>&visited,unordered_map<string,int>&supply,unordered_map<string,int>&res,vector<vector<string>>& ingredients,vector<string>&recipe){
+        bool check=true;
+        visited[recipe[i]]=0;
+        for(auto it:ingredients[i]){
+            if(supply.find(it)!=supply.end())continue;
+            if(res.find(it)!=res.end()){
+            if(visited.find(it)!=visited.end()){
+                if(visited[it]==0){
+                    check=false;
                 }
             }
+            else{
+                dfs(res[it],visited,supply,res,ingredients,recipe);
+                if(visited[it]==0)check=false;
+            }
+            }
+            else{
+                check=false;
+            }
+            if(check==false)break;
+        }
+        if(check)visited[recipe[i]]=1;
+        else visited[recipe[i]]=0;
+     }
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string,int>supply,visited,res;
+        for(auto it:supplies)supply[it]++;
+        for(int i=0;i<recipes.size();i++)res[recipes[i]]=i;
+        for(int i=0;i<recipes.size();i++){
+            if(visited.find(recipes[i])==visited.end()){
+                dfs(i,visited,supply,res,ingredients,recipes);
+            }
+        }
+        vector<string>ans;
+        for(auto it:visited){
+            if(it.second==1)ans.push_back(it.first);
         }
         return ans;
     }
