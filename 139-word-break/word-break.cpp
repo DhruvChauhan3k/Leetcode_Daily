c5ce230feacdf5192cb0d0910760fda2e8bd7499
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2026-02-06 22:02:35 +0530

    Time: 3 ms (69.22%) | Memory: 13.8 MB (52.40%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-bool check(int i,string &s, unordered_map<string,int>&m,string curr, vector<unordered_map<string,bool>>&dp)
-{
-    curr+=s[i];
-    if(dp[i].find(curr)!=dp[i].end())return dp[i][curr];
-    if(i==s.size()-1)
+    bool check(int i,string s,unordered_map<string,int>&m,vector<int>&dp)
     {
-        if(m.find(curr)!=m.end())return true;
-        return false;
+        if(i==s.size())return true;
+        if(dp[i]!=-1)return dp[i];
+        string temp;
+        for(int j=i;j<s.size();j++){
+          temp+=s[j];
+          if(m.find(temp)!=m.end()){
+            if(check(j+1,s,m,dp)){
+                return dp[i]=true;
+            }
+          }
+        }
+        return dp[i]=false;
     }
-    bool a=false;
-    if(m.find(curr)!=m.end())
-    {
-        a=check(i+1,s,m,"",dp);
-    }
-    bool b=check(i+1,s,m,curr,dp);
-    return dp[i][curr]= a or b;
-}
     bool wordBreak(string s, vector<string>& w) {
+        int n=s.size();
+        vector<int>dp(n,-1);
         unordered_map<string,int>m;
-        vector<unordered_map<string,bool>>dp(s.size());
         for(auto it:w)m[it]++;
-        string curr;
-        return check(0,s,m,curr,dp);
+        return check(0,s,m,dp);
     }
 };
