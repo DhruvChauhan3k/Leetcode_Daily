1f0c6b4aa12a67da0c522f5e92da715046013df0
Dhruv Chauhan <95209048+DhruvChauhan3k@users.noreply.github.com>
2026-02-08 19:23:01 +0530

    Time: 374 ms (13.19%) | Memory: 292 MB (7.35%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.empty()) return "";
-
-        int start = 0, maxLen = 0;
-        int n = s.size();
-
-        for (int center = 0; center < n; ++center) {
-            // Odd-length palindromes (center at s[center])
-            
-            expandAroundCenter(s, center, center, start, maxLen);
-            // Even-length palindromes (center between s[center] and s[center+1])
-            expandAroundCenter(s, center, center + 1, start, maxLen);
-        }
-
-        return s.substr(start, maxLen);
-    }
-
-private:
-    void expandAroundCenter(const string& s, int left, int right, int& start, int& maxLen) {
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            int len = right - left + 1;
-            if (len > maxLen) {
-                maxLen = len;
-                start = left;
+string memoization(string& s) {
+        vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
+        int len = 0;
+        int si=0,sj=0;
+        for(int i = 0; i < s.size(); ++i) {
+            for(int j = i; j < s.size(); ++j) {
+                if(mem[i][j]==-1)solve(mem,s,i,j);
+                if(mem[i][j]==1 and (j-i)+1>len){
+                    len=j-i+1;
+                    si=i;
+                    sj=j;
+                };
             }
-            --left;
-            ++right;
         }
+        string sans;
+        for(int i=si;i<=sj;i++)sans+=s[i];
+        return sans;
+    }
+    
+    int solve(vector<vector<int>>& mem, string& s, int i, int j) {
+        if (i >= j) return 1;
+        if(s[i]!=s[j])return 0;
+        if(mem[i][j]!=-1)return mem[i][j];
+        mem[i][j]=solve(mem,s,i+1,j-1);
+        return mem[i][j];
+    }
+    string longestPalindrome(string s) {
+        return memoization(s);
     }
 };
